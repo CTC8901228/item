b7040d4294520a9ca5cb4d88518e1122f987c645
CTC8901228 <114205447+CTC8901228@users.noreply.github.com>
2023-10-06 19:56:39 +0800

    Add files via upload

--- hw3.py
+++ hw3.py
@@ -41,7 +41,7 @@ try:
     for ch in testService.getCharacteristics():
         print(str(ch))
 
-    ch = dev.getCharacteristics(uuid=UUID(0xfff1))[0]
+    ch = dev.getCharacteristics(uuid=UUID(0xfff4))[0]
     if ch.supportsRead():
         print(ch.read())
 
