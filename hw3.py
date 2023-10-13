74b27af005cba2ec95e6e6cfa13e00caf4be0f8d
CTC8901228 <114205447+CTC8901228@users.noreply.github.com>
2023-10-13 17:22:11 +0800

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -43,7 +43,7 @@ for i in dev.getDescriptors(startHnd=1, endHnd=0x2909):
 
     print(i.uuid)
     if(i.uuid==0x2902):
-        i.write(0x1000)
+        i.write(1.to_bytes(1))
         print('write!!!!')
         
 
